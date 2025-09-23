1994c6411c1b5c59ef9dbfbf935704082cfef372
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2025-09-23 08:56:59 +0200

    Add configurator exercise base

--- src/Search/DatabaseConferenceSearch.php
+++ src/Search/DatabaseConferenceSearch.php
@@ -2,13 +2,32 @@
 
 namespace App\Search;
 
+use App\Entity\Organization;
 use App\Repository\ConferenceRepository;
 
 class DatabaseConferenceSearch implements ConferenceSearchInterface
 {
+    private ?Organization $organization = null;
+
+    private ?int $limit = null;
+
     public function __construct(
         private readonly ConferenceRepository $repository,
     ) {}
+
+    public function setOrganization(?Organization $organization): DatabaseConferenceSearch
+    {
+        $this->organization = $organization;
+
+        return $this;
+    }
+
+    public function setLimit(?int $limit): DatabaseConferenceSearch
+    {
+        $this->limit = $limit;
+
+        return $this;
+    }
 
     public function search(?string $name): array
     {
@@ -17,5 +36,14 @@ class DatabaseConferenceSearch implements ConferenceSearchInterface
         }
 
         return $this->repository->findLikeName($name);
+    }
+
+    public function fetchForOrganization(): iterable
+    {
+        if ($this->organization === null || $this->limit === null) {
+            throw new \LogicException('Missing configuration.');
+        }
+
+        return $this->repository->findBy(['organization' => $this->organization], limit: $this->limit);
     }
 }
